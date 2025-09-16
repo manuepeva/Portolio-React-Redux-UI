ccbffa7a395c8e3988c11771e641e83b351a3de3
Manuel Perez <manuepeva@gmail.com>
2025-09-16 20:07:19 +0200

    solving 401 errors

--- src/api/AxiosClient.js
+++ src/api/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'https://portfolio-server-21pcsyj1k-manuel-prez-s-projects.vercel.app',
+  baseURL: 'https://portfolio-server-7uo36pjh3-manuel-prez-s-projects.vercel.app/api',
   // baseURL: 'http://localhost:4000',
   headers: {
     "Content-Type": "application/json",
