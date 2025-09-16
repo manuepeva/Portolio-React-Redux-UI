d4047021f5308c4580cc681de1c454474142b138
Manuel Perez <manuepeva@gmail.com>
2025-09-16 19:42:00 +0200

    solving typos

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-verconst path = require('path');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
