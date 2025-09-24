0472f36e148ae76598edaed031e06374124ac48b
Manuel Perez <manuepeva@gmail.com>
2025-09-24 21:47:42 +0200

    adding updated backedn url

--- src/api/AxiosClient.js
+++ src/api/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'https://portfolio-server-7uo36pjh3-manuel-prez-s-projects.vercel.app/api',
+  baseURL: 'https://portfolio-server-hddu.onrender.com',
   // baseURL: 'http://localhost:4000',
   headers: {
     "Content-Type": "application/json",
